46b474d5ea1e9486cf8d34980ec47f04ba4a0f1e
Nemo Ingendaa <n.ingendaa@gmail.com>
2021-12-13 19:10:16 +0100

    Trying crossvalidation

--- project.py
+++ project.py
@@ -1,26 +1,154 @@
 # importing the required module
 import numpy as np
+from numpy.core.numeric import cross
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.core.frame import DataFrame
+from sklearn import tree
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 # import seaborn as sns
 # sns.set_style('whitegrid')
 # import statsmodels.api as sm
 
 # https://colab.research.google.com/github/meizmyang/Student-Performance-Classification-Analysis/blob/master/Student%20Performance%20Analysis%20and%20Classification.ipynb#scrollTo=IdD7SgZoHjd9
-mat = pd.read_csv("Dataset/student-mat.csv", sep=';') # load dataset of the math classes 
 
-mat.columns = ['school','sex','age','address','familySize','parentsStatus','motherEducation','fatherEducation',
-           'motherJob','fatherJob','reason','guardian','commuteTime','studyTime','failures','schoolSupport',
-          'familySupport','paidClasses','activities','nursery','desireHigherEdu','internet','romantic','familyQuality',
-          'freeTime','goOutFriends','workdayAlc','weekendAlc','health','absences','1stPeriod','2ndPeriod','final']
- 
-mat['finalGrade'] = 'None'
-mat.loc[(mat.final >= 15) & (mat.final <= 20), 'finalGrade'] = 'good' 
-mat.loc[(mat.final >= 10) & (mat.final <= 14), 'finalGrade'] = 'fair' 
-mat.loc[(mat.final >= 0) & (mat.final <= 9), 'finalGrade'] = 'poor' 
-mat.head(5)
+def prepareForDecisionTree(X : DataFrame):
+    X['school'] = [0 if i == 'GP' else 1 for i in X['school']] # GP = 0, MS = 1
+    X['sex'] = [0 if i == 'M' else 1 for i in X['sex']] # M = 0, F = 1
+    X['address'] = [0 if i == 'U' else 1 for i in X['address']] # U = 0, R = 1
+    X['familySize'] = [0 if i == 'LE3' else 1 for i in X['familySize']] # LE3 = 0, GE3 = 1
+    X['parentsStatus'] = [0 if i == 'T' else 1 for i in X['parentsStatus']] # T = 0, A = 1
+    X['schoolSupport'] = [0 if i == 'no' else 1 for i in X['schoolSupport']] # no = 0, yes = 1
+    X['familySupport'] = [0 if i == 'no' else 1 for i in X['familySupport']] # no = 0, yes = 1
+    X['paidClasses'] = [0 if i == 'no' else 1 for i in X['paidClasses']] # no = 0, yes = 1
+    X['activities'] = [0 if i == 'no' else 1 for i in X['activities']] # no = 0, yes = 1
+    X['nursery'] = [0 if i == 'no' else 1 for i in X['nursery']] # no = 0, yes = 1
+    X['desireHigherEdu'] = [0 if i == 'no' else 1 for i in X['desireHigherEdu']] # no = 0, yes = 1
+    X['internet'] = [0 if i == 'no' else 1 for i in X['internet']] # no = 0, yes = 1
+    X['romantic'] = [0 if i == 'no' else 1 for i in X['romantic']] # no = 0, yes = 1
+
+    list = []
+    for i in X['motherJob'] : # other = 0, at_home = 1, services = 2, health = 3, teacher = 4
+        if i == 'at_home':
+            list.append(1)
+        elif i == 'services':
+            list.append(2)
+        elif i == 'health':
+            list.append(3)
+        elif i == 'teacher':
+            list.append(4)
+        else:
+            list.append(0)
+    X['motherJob'] = list
+
+    list = []
+    for i in X['fatherJob'] : # other = 0, at_home = 1, services = 2, health = 3, teacher = 4
+        if i == 'at_home':
+            list.append(1)
+        elif i == 'services':
+            list.append(2)
+        elif i == 'health':
+            list.append(3)
+        elif i == 'teacher':
+            list.append(4)
+        else:
+            list.append(0)
+    X['fatherJob'] = list
+
+    list = []
+    for i in X['reason'] : # other = 0, course = 1, reputation = 2, home = 3
+        if i == 'course':
+            list.append(1)
+        elif i == 'reputation':
+            list.append(2)
+        elif i == 'home':
+            list.append(3)
+        else:
+            list.append(0)
+    X['reason'] = list
+
+    list = []
+    for i in X['guardian'] : # other = 0, father = 1, mother = 2
+        if i == 'father':
+            list.append(1)
+        elif i == 'mother':
+            list.append(2)
+        else:
+            list.append(0)
+    X['guardian'] = list
+
+    X = X.values.tolist() # make X into a list(needed for creating a decision tree)
+    X = np.asarray(X)
+    return X
+
+def crossValidation(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1234)
+    nr_of_splits = 10
+    train_error_list_stratified = np.zeros((nr_of_splits,20))
+    test_error_list_stratified = np.zeros((nr_of_splits,20))
+    for i, (train, test) in enumerate(StratifiedKFold(n_splits=nr_of_splits).split(X,y) ):
+        X_train = X[train]
+        y_train = y[train]
+        X_test = X[test]
+        y_test = y[test]
+        for j in range (2,20):
+            dtc =  tree.DecisionTreeClassifier(max_depth = j)
+            dtc = dtc.fit(X_train, y_train)
+            y_train_pred = dtc.predict(X_train)
+            y_test_pred = dtc.predict(X_test)
+            train_error = 1 - accuracy_score(y_train, y_train_pred)
+            train_error_list_stratified[i, j-2] = train_error
+            test_error = 1 - accuracy_score(y_test, y_test_pred)
+            test_error_list_stratified[i, j-2] = test_error 
+        
+    plt.title('Stratified classification error, train vs test')
+    plt.plot(train_error_list_stratified.mean(0), label = "train")
+    plt.plot(test_error_list_stratified.mean(0),label = "test")
+    plt.ylabel('Classification error')
+    plt.xlabel('Depth')
+    plt.legend()
+    plt.show()
+    
+
+def main():
+    mat = pd.read_csv("Dataset/student-mat.csv", sep=';') # load dataset of the math classes 
+
+    mat.columns = ['school','sex','age','address','familySize','parentsStatus','motherEducation','fatherEducation',
+            'motherJob','fatherJob','reason','guardian','commuteTime','studyTime','failures','schoolSupport',
+            'familySupport','paidClasses','activities','nursery','desireHigherEdu','internet','romantic','familyQuality',
+            'freeTime','goOutFriends','workdayAlc','weekendAlc','health','absences','1stPeriod','2ndPeriod','final']
+
+    mat['finalGrade'] = 'None'
+    mat.loc[(mat.final >= 15) & (mat.final <= 20), 'finalGrade'] = 2
+    mat.loc[(mat.final >= 10) & (mat.final <= 14), 'finalGrade'] = 1
+    mat.loc[(mat.final >= 0) & (mat.final <= 9), 'finalGrade'] = 0
+
+    X = mat.copy(deep=True) # coping mat without changing the original when X is changed
+
+    y = X['finalGrade'].ravel()
+    y = y.astype('int')
+
+    X.drop(['1stPeriod','2ndPeriod','final','finalGrade' ], axis=1, inplace=True)
+
+    X = prepareForDecisionTree(X)
+    y = np.asarray(y)
+    attribute_names = mat.columns
+    class_names = ['poor', 'fair', 'good']
+    
+    crossValidation(X,y)
+    # dtc = tree.DecisionTreeClassifier(min_samples_split=2)
+    # dtc = dtc.fit(X, y)
+    # plt.figure(figsize=(150,80), dpi=80)
+    # # plt.figure(figsize=(10,5), dpi=80)
+
+    # tree.plot_tree(dtc, feature_names = attribute_names, class_names = class_names, filled =True)
+    # plt.savefig("decisiontree.png")
 
 
-df = pd.DataFrame(mat)
-df.plot()
-plt.show()
+    
+
+
+if __name__ == '__main__':
+    main()
